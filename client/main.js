734f17f8d1ecc530d9a871b3c3b9e5c56122b938
SavenokNikolay <savenok.kolya.nikolay@gmail.com>
2023-07-25 17:57:18 +0300

    first commit

---
+++ client/main.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const addEvent = document.querySelector('.btn')
+addEvent.addEventListener('click', () => {
+   fetch('/questions')
+      .then(response => response.json())
+      .then(questions => {
+
+         const questionsContainer = document.querySelector('.questions');
+
+         // Для кожного питання створюємо відповідні елементи форми
+         questions.forEach((question, index) => {
+            const questionElement = document.createElement('div');
+            questionElement.className = 'questions__form'
+            questionElement.innerHTML = `
+                           <p>Питання ${index + 1}: ${question.caption}</p>
+                           <input type="radio" name="answer_${index}" value="true"> Так
+                           <input type="radio" name="answer_${index}" value="false"> Ні
+                       `;
+            questionsContainer.appendChild(questionElement);
+         });
+
+         // Включаємо кнопку "Перевірити" після того, як всі відповіді проставлені
+         const checkButton = document.querySelector('.check-button');
+         const answerInputs = document.querySelectorAll('input[type="radio"]');
+         answerInputs.forEach(input => {
+            input.addEventListener('change', () => {
+               const allAnswersSelected = Array.from(answerInputs).every(input => input.checked);
+               checkButton.disabled = !allAnswersSelected;
+            });
+         });
+
+         // Обробка натискання на кнопку "Перевірити"
+         checkButton.addEventListener('click', () => {
+            const answers = Array.from(answerInputs).map(input => input.checked);
+            const requestOptions = {
+               method: 'POST',
+               headers: {
+                  'Content-Type': 'application/json'
+               },
+               body: JSON.stringify(answers)
+            };
+
+            fetch('/check', requestOptions)
+               .then(response => response.json())
+               .then(result => {
+                  alert(`Ваш результат: ${result.score} правильних відповідей з ${questions.length}`);
+               })
+               .catch(error => {
+                  console.error('Помилка під час перевірки відповідей:', error);
+               });
+         });
+
+      })
+      .catch(error => {
+         console.error('Помилка під час отримання питань з сервера:', error);
+      });
+})
---
+++ server/index.mjs
@@ -0,0 +1,63 @@
+import fastifyStatic from '@fastify/static';
+import fastify from 'fastify'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const server = fastify();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const questions = [
+   {
+      caption: 'Подія натискання на елемент називається click?',
+      correctAnswer: true
+   },
+   {
+      caption: 'Усередині розмітки не можна додати обробник події?',
+      correctAnswer: false
+   },
+   {
+      caption: 'Припинити спливання події можна за допомогою метода stopImmediatePropagation?',
+      correctAnswer: false
+   },
+   {
+      caption: 'Припинити спливання події можна за допомогою метода stopPropagation?',
+      correctAnswer: true
+   }
+];
+
+server.register(fastifyStatic, {
+   root: path.join(__dirname, '../client')
+ })
+
+server.get('/questions', (request, reply) => {
+   return reply.send(questions);
+});
+
+server.post('/check', (request, reply) => {
+   const userAnswers = request.body;
+
+   // Перевіряємо правильність відповідей та рахуємо кількість правильних
+   let score = 0;
+   for (let i = 0; i < questions.length; i++) {
+      if (userAnswers[i] === questions[i].correctAnswer) {
+         score++;
+      }
+   }
+
+   const result = {
+      score: score,
+      totalQuestions: questions.length
+   };
+
+   reply.send(result);
+});
+
+server.listen({ port: 5556 })
+   .then(() => {
+      console.log('Server started')
+   })
+   .catch((error) => {
+      console.log('Error', error)
+   });
+
